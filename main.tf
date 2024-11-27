bc3b968f0766b9dce0752e9c8616de1dd109ac2c
Filipe Borba <borba.filipe00@gmail.com>
2024-11-27 15:22:24 -0300

    feat: adicionando multiplas instancias ao banco mysql

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-##MYSQL
 
 provider "aws" {
   region = "us-east-1"
 }
 
-# Obter VPC padrão
 data "aws_vpc" "default" {
   default = true
 }
-
-# Obter todas as sub-redes da VPC padrão
 data "aws_subnets" "default_subnets" {
   filter {
     name   = "vpc-id"
@@ -17,27 +13,25 @@ data "aws_subnets" "default_subnets" {
   }
 }
 
-# Criar grupo de sub-rede para o RDS
 resource "aws_db_subnet_group" "rds_db_subnet_group" {
   name       = "rds-db-subnet-group"
-  subnet_ids = slice(data.aws_subnets.default_subnets.ids, 0, 2) # Pega as duas primeiras sub-redes
+  subnet_ids = slice(data.aws_subnets.default_subnets.ids, 0, 2)
 
   tags = {
     Name = "rds-db-subnet-group"
   }
 }
 
-# Criar grupo de segurança para o RDS MySQL
 resource "aws_security_group" "rds_mysql_sg" {
   vpc_id      = data.aws_vpc.default.id
   name        = "rds-mysql-sg"
   description = "Grupo de seguranca para RDS MySQL"
-  
+
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -47,96 +41,87 @@ resource "aws_security_group" "rds_mysql_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-# Criar a instância RDS MySQL
-resource "aws_db_instance" "default" {
-  identifier         = "mysql-ftc3"
-  engine             = "mysql"
-  engine_version     = "8.0.35" 
-  instance_class     = "db.t3.micro"
-  allocated_storage   = 20
-  storage_type       = "gp2"
-  username           = var.db_username
-  password           = var.db_password
-  db_name            = var.db_name
-  skip_final_snapshot = true
-  publicly_accessible = true
+resource "aws_db_instance" "product" {
+  identifier             = "mysql-ftc4-product"
+  engine                 = "mysql"
+  engine_version         = "8.0.35"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = "product"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.rds_mysql_sg.id]
-  db_subnet_group_name = aws_db_subnet_group.rds_db_subnet_group.name
+  db_subnet_group_name   = aws_db_subnet_group.rds_db_subnet_group.name
 }
 
-##MONGO
-
+resource "aws_db_instance" "order_db" {
+  identifier             = "mysql-ftc4-order"
+  engine                 = "mysql"
+  engine_version         = "8.0.35"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = "order_db"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.rds_mysql_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_db_subnet_group.name
+}
+resource "aws_db_instance" "payment" {
+  identifier             = "mysql-ftc4-payment"
+  engine                 = "mysql"
+  engine_version         = "8.0.35"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  username               = var.db_username
+  password               = var.db_password
+  db_name                = "payment"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.rds_mysql_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds_db_subnet_group.name
+}
 locals {
-  # Replace ORG_ID, PUBLIC_KEY and PRIVATE_KEY with your Atlas variables
-  mongodb_atlas_api_pub_key = "rmxsviid"
-  mongodb_atlas_api_pri_key = "e7fead49-8ea9-4120-afe1-d0b08bf1d7d1"
-  mongodb_atlas_org_id  = "65f23cc56dd714486d541a44"
-
-  # Replace USERNAME And PASSWORD with what you want for your database user
-  # https://docs.atlas.mongodb.com/tutorial/create-mongodb-user-for-cluster/
-  mongodb_atlas_database_username = "root"
+  mongodb_atlas_api_pub_key            = "rmxsviid"
+  mongodb_atlas_api_pri_key            = "e7fead49-8ea9-4120-afe1-d0b08bf1d7d1"
+  mongodb_atlas_org_id                 = "65f23cc56dd714486d541a44"
+  mongodb_atlas_database_username      = "root"
   mongodb_atlas_database_user_password = "root"
-
-  # Replace IP_ADDRESS with the IP Address from where your application will connect
-  # https://docs.atlas.mongodb.com/security/add-ip-address-to-list/
-  mongodb_atlas_accesslistip = "192.168.0.103"
+  mongodb_atlas_accesslistip           = "0.0.0.0"
 }
-
-#
-# Configure the MongoDB Atlas Provider
-#
 terraform {
   required_providers {
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
     }
   }
- }
+}
 
 provider "mongodbatlas" {
   public_key  = local.mongodb_atlas_api_pub_key
   private_key = local.mongodb_atlas_api_pri_key
 }
-
-#
-# Create a Project
-#
 resource "mongodbatlas_project" "my_project" {
-  name   = "cliente"
+  name   = "cliente-project"
   org_id = local.mongodb_atlas_org_id
 }
-
-#
-# Create a Shared Tier Cluster
-#
 resource "mongodbatlas_cluster" "my_cluster" {
-  project_id              = mongodbatlas_project.my_project.id
-  name                    = "cliente-cluster"
-
-  # Provider Settings "block"
-  provider_name = "TENANT"
-
-  # options: AWS AZURE GCP
-  backing_provider_name = "AWS"
-
-  # options: M2/M5 atlas regions per cloud provider
-  # GCP - CENTRAL_US SOUTH_AMERICA_EAST_1 WESTERN_EUROPE EASTERN_ASIA_PACIFIC NORTHEASTERN_ASIA_PACIFIC ASIA_SOUTH_1
-  # AZURE - US_EAST_2 US_WEST CANADA_CENTRAL EUROPE_NORTH
-  # AWS - US_EAST_1 US_WEST_2 EU_WEST_1 EU_CENTRAL_1 AP_SOUTH_1 AP_SOUTHEAST_1 AP_SOUTHEAST_2
-  provider_region_name = "US_EAST_1"
-
-  # options: M2 M5
-  provider_instance_size_name = "M0"
-
-  # Will not change till new version of MongoDB but must be included
-  mongo_db_major_version = "4.4"
+  project_id                   = mongodbatlas_project.my_project.id
+  name                         = "cliente-cluster"
+  provider_name                = "TENANT"
+  backing_provider_name        = "AWS"
+  provider_region_name         = "US_EAST_1"
+  provider_instance_size_name  = "M0"
+  mongo_db_major_version       = "4.4"
   auto_scaling_disk_gb_enabled = "false"
 }
 
-#
-# Create an Atlas Admin Database User
-#
 resource "mongodbatlas_database_user" "my_user" {
   username           = local.mongodb_atlas_database_username
   password           = local.mongodb_atlas_database_user_password
@@ -149,20 +134,8 @@ resource "mongodbatlas_database_user" "my_user" {
   }
 }
 
-#
-# Create an IP Accesslist
-#
-# can also take a CIDR block or AWS Security Group -
-# replace ip_address with either cidr_block = "CIDR_NOTATION"
-# or aws_security_group = "SECURITY_GROUP_ID"
-#
 resource "mongodbatlas_project_ip_access_list" "my_ipaddress" {
-      project_id = mongodbatlas_project.my_project.id
-      ip_address = local.mongodb_atlas_accesslistip
-      comment    = "My IP Address"
-}
-
-# Use terraform output to display connection strings.
-output "connection_strings" {
-  value = mongodbatlas_cluster.my_cluster.connection_strings.0.standard_srv
+  project_id = mongodbatlas_project.my_project.id
+  ip_address = local.mongodb_atlas_accesslistip
+  comment    = "My IP Address"
 }
